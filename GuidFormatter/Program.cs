1d86c8cc199bfd99ac14c7df2b494114f7c159e5
Rubens Lopes <github@rubenslop.es>
2024-11-22 23:11:24 -0400

    initial

---
+++ GuidFormatter/Program.cs
@@ -0,0 +1,10 @@
+﻿using Cocona;
+
+using TextCopy;
+
+CoconaApp.Run(([Argument] string guid) =>
+{
+    var result = Guid.Parse(guid).ToString();
+    Console.WriteLine(result);
+    ClipboardService.SetText(result);
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# RAL Utilities
+A collection of useful, at least to me, stuff
